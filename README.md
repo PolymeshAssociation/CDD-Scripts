c638220777924775dab59553913aa0b3e873b93c
Adam Dossa <adamdossa@Adams-MacBook-Pro.local>
2020-12-27 16:49:13 +0000

    Add initial scripts

---
+++ README.md
@@ -0,0 +1,269 @@
+# CDD Scripts
+
+This project provides sample, non-production, scripts for common CDD provider use-cases.
+
+It uses:  
+- Polymesh Blockchain
+- Polymesh SDK
+- PUIS
+- Confidential Library
+
+Note - this project does not currently make use of the PUIS Audit System.
+
+## Use Cases
+
+There are three common use-cases for CDD providers.
+
+In all cases, the project assumes:  
+- CDD provider has verified user matches PII data (via documentation checks)  
+- CDD provider has verified that user owns Primary Key (via random signing challenge)
+
+In all cases, the CDD provider must supply the users:  
+- Primary Key
+- Hashed PII data 
+
+### New user
+
+In this scenario, the user has not interacted with Polymesh previously.
+
+They have no existing identity on Polymesh, nor are they registered in the PUIS.
+
+If the users PII data is unique within the PUIS, a new identity is created, the PII data is registered to the PUIS, and a CDD claim is attached to the users new identity.
+
+If the users PII data is not unique within the PUIS, a redirect response is returned, indicating the user must go to another CDD provider.
+
+```
+// New User - no existing uID, new identity
+//
+// Behaviour:
+//  If primary_key is already in use, return failure
+//  If pii_hash exists, should return redirect
+//  Otherwise generates unique id, creates new identity and attaches CDD claim
+//
+// Assumptions:
+//  CDD provider has verified user matches pii_hash data (via documentation checks)
+//  CDD provider has verified that user owns primary_key (via random signing challenge)
+//
+// Returns:
+//  1st value indicates whether operation was successful, or whether a redirect is needed
+//  [true, did, v4_uuid] or [false, redirect_provider_name, redirect_provider_url]
+//  otherwise throws error
+async function new_user(primary_key, pii_hash)
+```
+
+### Existing user with an existing identity
+
+In this scenario, the user has already been onboarded to Polymesh and is asking for a new CDD claim on an existing identity.
+
+In this case, the CDD provider must also provide the users unique id and the DID against which the new CDD claim will be written, both supplied by the user.
+
+```
+// Existing User - existing uID, existing identity
+//
+// Behaviour:
+//  If pii_hash does not match v4_unique_id, return failure
+//  If did does not exist on Polymesh, return failure
+//  If v4_unique_id does not match existing valid CDD claims on did, return failure (TODO)
+//  Otherwise attaches CDD claim
+//
+// Assumptions:
+//  CDD provider has verified user matches pii_hash data (via documentation checks)
+//  CDD provider has verified that user owns primary_key (via random signing challenge)
+//
+// Returns:
+//  true if operation was successful, otherwise throws error
+async function existing_user_with_existing_identity(primary_key, pii_hash, v4_unique_id, did)
+```
+
+### Existing user with an new identity
+
+In this scenario, the user has already been onboarded to Polymesh and is asking for a new identity to be created with a new CDD claim.
+
+In this case, the CDD provider must also provide the users unique id as supplied by the user.
+
+```
+// Existing User - existing uID, new identity
+//
+// Behaviour:
+//  If primary_key is already in use, return failure
+//  If pii_hash does not match v4_unique_id, return failure
+//  Otherwise creates new identity and attaches CDD claim
+//
+// Assumptions:
+//  CDD provider has verified user matches pii_hash data (via documentation checks)
+//  CDD provider has verified that user owns primary_key (via random signing challenge)
+//
+// Returns:
+//  [true, did] if operation is successful, otherwise throws error
+async function existing_user_with_new_identity(primary_key, pii_hash, v4_unique_id)
+```
+
+## Sample Output
+
+A sample script can be executed using:  
+```
+yarn
+node ./index.js
+```
+
+The output should look similar to:  
+```
+Creating New User: 
+ {
+  "primary_key": "5GUKzjVpS7cLgKPnPskmq6xhCAx4YWqo9DgFQ8DFdvabMn3f",
+  "pii_hash": [
+    {
+      "check_name": "BaseIndividual",
+      "pii_payloads": [
+        {
+          "payload_name": "FirstName",
+          "payload_value": "a94967581376bccb35cc923ea136892c734782b028310d176c616998e56c4b0f743a15364f4ad371cb9c22fd4a0a16d77e338eab396fc87fcc83e442b08d976d"
+        },
+        {
+          "payload_name": "LastName",
+          "payload_value": "6be670010f83dbe40fec70305830d133a84879fe776890eee94f6101ce4b2e58c4186b963000bc9fc82e4d9636804286bf7d0fe327e7fdb9ff2ca5923e7627f1"
+        },
+        {
+          "payload_name": "BirthDate",
+          "payload_value": "af8709cc9d99fe82c7d0d5b81775e4397e909b7cdf39c246152fafc991670cda4491d7e03befd4e151f2b5a1bc8748c5f5a4ad67edfd34073e09edbccd77bbaa"
+        },
+        {
+          "payload_name": "CountryAlpha2",
+          "payload_value": "CA"
+        }
+      ]
+    }
+  ]
+}
+****Polymesh***** Getting SDK api
+****Polymesh***** Checking Primary Key is unused
+Primary Key is unused
+****PUIS***** Getting Swagger Client
+****PUIS***** Checking if PII hash exists
+PII hash does not exist
+****PUIS***** Generating uID
+uID Generated:  7b45a874-a106-4057-b82e-f47cb42ccad7
+****Polymesh***** Creating DID
+DID Created:  0xe68edc3585d89142240bca208e51b774f5400570119b9d3120f2f3b567b37095
+****Confidential Identity Library***** Deriving CDD_ID
+CDD_ID Derived:  0x9444ce1ca187d60c77d31329160a236ecf7a1a65deba1c32a750baeb676fb673
+****Polymesh***** Writing CDD Attestation
+CDD Attestation Written
+New User:  0xe68edc3585d89142240bca208e51b774f5400570119b9d3120f2f3b567b37095 7b45a874-a106-4057-b82e-f47cb42ccad7
+
+Redirecting New User: 
+ {
+  "primary_key": "5FpoC8Nsoy2Ur3vHobch4do9igSCADNdUBwStJoYXN6Eib1c",
+  "pii_hash": [
+    {
+      "check_name": "BaseIndividual",
+      "pii_payloads": [
+        {
+          "payload_name": "FirstName",
+          "payload_value": "a94967581376bccb35cc923ea136892c734782b028310d176c616998e56c4b0f743a15364f4ad371cb9c22fd4a0a16d77e338eab396fc87fcc83e442b08d976d"
+        },
+        {
+          "payload_name": "LastName",
+          "payload_value": "6be670010f83dbe40fec70305830d133a84879fe776890eee94f6101ce4b2e58c4186b963000bc9fc82e4d9636804286bf7d0fe327e7fdb9ff2ca5923e7627f1"
+        },
+        {
+          "payload_name": "BirthDate",
+          "payload_value": "af8709cc9d99fe82c7d0d5b81775e4397e909b7cdf39c246152fafc991670cda4491d7e03befd4e151f2b5a1bc8748c5f5a4ad67edfd34073e09edbccd77bbaa"
+        },
+        {
+          "payload_name": "CountryAlpha2",
+          "payload_value": "CA"
+        }
+      ]
+    }
+  ]
+}
+****Polymesh***** Getting SDK api
+****Polymesh***** Checking Primary Key is unused
+Primary Key is unused
+****PUIS***** Getting Swagger Client
+****PUIS***** Checking if PII hash exists
+Redirected User:  Polymath https://polymath.network/
+
+Creating Second Claim On Existing DID: 
+ {
+  "primary_key": "5GUKzjVpS7cLgKPnPskmq6xhCAx4YWqo9DgFQ8DFdvabMn3f",
+  "pii_hash": [
+    {
+      "check_name": "BaseIndividual",
+      "pii_payloads": [
+        {
+          "payload_name": "FirstName",
+          "payload_value": "a94967581376bccb35cc923ea136892c734782b028310d176c616998e56c4b0f743a15364f4ad371cb9c22fd4a0a16d77e338eab396fc87fcc83e442b08d976d"
+        },
+        {
+          "payload_name": "LastName",
+          "payload_value": "6be670010f83dbe40fec70305830d133a84879fe776890eee94f6101ce4b2e58c4186b963000bc9fc82e4d9636804286bf7d0fe327e7fdb9ff2ca5923e7627f1"
+        },
+        {
+          "payload_name": "BirthDate",
+          "payload_value": "af8709cc9d99fe82c7d0d5b81775e4397e909b7cdf39c246152fafc991670cda4491d7e03befd4e151f2b5a1bc8748c5f5a4ad67edfd34073e09edbccd77bbaa"
+        },
+        {
+          "payload_name": "CountryAlpha2",
+          "payload_value": "CA"
+        }
+      ]
+    }
+  ],
+  "uuid": "7b45a874-a106-4057-b82e-f47cb42ccad7",
+  "did": "0xe68edc3585d89142240bca208e51b774f5400570119b9d3120f2f3b567b37095"
+}
+****Polymesh***** Getting SDK api
+****Polymesh***** Checking Primary Key matches DID
+Primary Key matches DID
+****PUIS***** Getting Swagger Client
+****PUIS***** Checking if PII hash matches unique id
+PII hash matches unique id
+****Confidential Identity Library***** Deriving CDD_ID
+CDD_ID Derived:  0x9444ce1ca187d60c77d31329160a236ecf7a1a65deba1c32a750baeb676fb673
+****Polymesh***** Writing CDD Attestation
+CDD Attestation Written
+Existing User, Existing Identity:  0xe68edc3585d89142240bca208e51b774f5400570119b9d3120f2f3b567b37095 7b45a874-a106-4057-b82e-f47cb42ccad7
+
+Creating Second Claim On New DID: 
+ {
+  "primary_key": "5FpoC8Nsoy2Ur3vHobch4do9igSCADNdUBwStJoYXN6Eib1c",
+  "pii_hash": [
+    {
+      "check_name": "BaseIndividual",
+      "pii_payloads": [
+        {
+          "payload_name": "FirstName",
+          "payload_value": "a94967581376bccb35cc923ea136892c734782b028310d176c616998e56c4b0f743a15364f4ad371cb9c22fd4a0a16d77e338eab396fc87fcc83e442b08d976d"
+        },
+        {
+          "payload_name": "LastName",
+          "payload_value": "6be670010f83dbe40fec70305830d133a84879fe776890eee94f6101ce4b2e58c4186b963000bc9fc82e4d9636804286bf7d0fe327e7fdb9ff2ca5923e7627f1"
+        },
+        {
+          "payload_name": "BirthDate",
+          "payload_value": "af8709cc9d99fe82c7d0d5b81775e4397e909b7cdf39c246152fafc991670cda4491d7e03befd4e151f2b5a1bc8748c5f5a4ad67edfd34073e09edbccd77bbaa"
+        },
+        {
+          "payload_name": "CountryAlpha2",
+          "payload_value": "CA"
+        }
+      ]
+    }
+  ],
+  "uuid": "7b45a874-a106-4057-b82e-f47cb42ccad7"
+}
+****Polymesh***** Getting SDK api
+****Polymesh***** Checking Primary Key is unused
+****PUIS***** Getting Swagger Client
+****PUIS***** Checking if PII hash matches unique id
+PII hash matches unique id
+****Polymesh***** Creating DID
+DID Created:  0x8b74d850dcf9676d49d3f514603d4fd15b7d794afe75ed01eaf60277bcc6ec3c
+****Confidential Identity Library***** Deriving CDD_ID
+CDD_ID Derived:  0xe459a935fc228195f16e381829d08bf9e42f8700afa51f4e18ee31b24612e643
+****Polymesh***** Writing CDD Attestation
+CDD Attestation Written
+Existing User, New Identity:  0xe68edc3585d89142240bca208e51b774f5400570119b9d3120f2f3b567b37095 7b45a874-a106-4057-b82e-f47cb42ccad7
+```
---
+++ index.js
@@ -0,0 +1,435 @@
+const { Polymesh } = require('@polymathnetwork/polymesh-sdk');
+const { ClaimType } = require('@polymathnetwork/polymesh-sdk/types');
+const { encodeAddress } = require('@polkadot/keyring');
+const SwaggerClient = require('swagger-client');
+const BigNumber = require('bignumber.js');
+const fs = require('fs');
+const path = require('path');
+const sha512 = require('js-sha512');
+const crypto = require('crypto');
+const confidential_identity = require('./pkg/confidential_identity_wasm');
+const { u8aToHex, hexToU8a, stringToU8a } = require('@polkadot/util');
+
+// Mock CDD Process
+// NB - does not implement PUIS Audit Process
+
+// Assumptions:
+//  CDD provider has verified user matches pii_hash data (via documentation checks)
+//  CDD provider has verified that user owns primary_key (via random signing challenge)
+
+// New User - no existing uID, new identity
+//
+// Behaviour:
+//  If primary_key is already in use, return failure
+//  If pii_hash exists, should return redirect
+//  Otherwise generates unique id, creates new identity and attaches CDD claim
+//
+// Assumptions:
+//  CDD provider has verified user matches pii_hash data (via documentation checks)
+//  CDD provider has verified that user owns primary_key (via random signing challenge)
+//
+// Returns:
+//  1st value indicates whether operation was successful, or whether a redirect is needed
+//  [true, did, v4_uuid] or [false, redirect_provider_name, redirect_provider_url]
+//  otherwise throws error
+async function new_user(primary_key, pii_hash) {
+
+    // console.log("****Input Data****");
+    // console.log(JSON.stringify({primary_key: primary_key, pii_hash: pii_hash}, null, 2));
+
+    console.log('****Polymesh***** Getting SDK api')
+    const polymesh = await getPolymesh();    
+
+    console.log('****Polymesh***** Checking Primary Key is unused')
+    let existing_identity = await getIdentityFromAccount(polymesh, primary_key);
+    if (existing_identity != undefined) {
+        throw new Error("PrimaryKeyInUse");
+    }
+    console.log("Primary Key is unused");
+
+    console.log('****PUIS***** Getting Swagger Client');
+    let puis = await getPUIS();
+
+    // Check if pii_hash already exists, and we need to redirect
+    console.log('****PUIS***** Checking if PII hash exists');
+    let [redirect, provider_name, provider_url] = await piiExists(puis, pii_hash);
+    if (redirect) {
+        return [false, provider_name, provider_url];
+    }
+    console.log('PII hash does not exist');
+
+    console.log('****PUIS***** Generating uID');
+    let v4_unique_id = await getUid(puis, pii_hash);
+    console.log("uID Generated: ", v4_unique_id);
+    // Change uID from uuid v4 format to hex format
+    let unique_id = "0x" + (v4_unique_id.replace(/-/g, ''));
+
+    console.log('****Polymesh***** Creating DID')
+    let identity = await createIdentity(polymesh, primary_key);
+    console.log("DID Created: ", identity.did);
+
+    console.log('****Confidential Identity Library***** Deriving CDD_ID');
+    let cdd_id = confidential_identity.process_create_cdd_id(
+        JSON.stringify({
+            investor_did: Array.from(hexToU8a(identity.did)),
+            investor_unique_id: Array.from(hexToU8a(unique_id))
+        })
+    );
+    cdd_id = "0x" + createHexString(JSON.parse(cdd_id).cdd_id);
+    console.log("CDD_ID Derived: ", cdd_id)
+
+    console.log('****Polymesh***** Writing CDD Attestation')
+    await writeCddClaim(polymesh, identity.did, cdd_id)
+    console.log('CDD Attestation Written')
+
+    return [true, identity.did, v4_unique_id];
+
+}
+
+// Existing User - existing uID, existing identity
+//
+// Behaviour:
+//  If pii_hash does not match v4_unique_id, return failure
+//  If did does not exist on Polymesh, return failure
+//  If v4_unique_id does not match existing valid CDD claims on did, return failure (TODO)
+//  Otherwise attaches CDD claim
+//
+// Assumptions:
+//  CDD provider has verified user matches pii_hash data (via documentation checks)
+//  CDD provider has verified that user owns primary_key (via random signing challenge)
+//
+// Returns:
+//  true if operation was successful, otherwise throws error
+async function existing_user_with_existing_identity(primary_key, pii_hash, v4_unique_id, did) {
+
+    // console.log("****Input Data****");
+    // console.log(JSON.stringify({pii_hash: pii_hash, unique_id: v4_unique_id, did: did}, null, 2));
+
+    console.log('****Polymesh***** Getting SDK api')
+    const polymesh = await getPolymesh();
+
+    console.log('****Polymesh***** Checking Primary Key matches DID')
+    let existing_identity = await getIdentityFromAccount(polymesh, primary_key);
+    if (existing_identity == undefined || existing_identity.did != did) {
+        throw Error("PrimaryKeyAndDidMismatch")
+    }
+    console.log("Primary Key matches DID");
+
+    console.log('****PUIS***** Getting Swagger Client');
+    let puis = await getPUIS();
+
+    // Check that verified pii data matches user provided unique id
+    console.log('****PUIS***** Checking if PII hash matches unique id');
+    let [piiMatch, provider_name, provider_url] = await piiHashMatchesUid(puis, pii_hash, v4_unique_id);
+    if (!piiMatch) {
+        throw Error("PiiAndUniqueIdMismatch")
+    }
+    console.log('PII hash matches unique id');
+    
+    let unique_id = "0x" + (v4_unique_id.replace(/-/g, ''));
+
+    console.log('****Confidential Identity Library***** Deriving CDD_ID');
+    let cdd_id = confidential_identity.process_create_cdd_id(
+        JSON.stringify({
+            investor_did: Array.from(hexToU8a(did)),
+            investor_unique_id: Array.from(hexToU8a(unique_id))
+        })
+    );
+    cdd_id = "0x" + createHexString(JSON.parse(cdd_id).cdd_id);
+    console.log("CDD_ID Derived: ", cdd_id) 
+
+    console.log('****Polymesh***** Writing CDD Attestation')
+    await writeCddClaim(polymesh, did, cdd_id)
+    console.log('CDD Attestation Written');
+
+    return true;
+
+}
+
+// Existing User - existing uID, new identity
+//
+// Behaviour:
+//  If primary_key is already in use, return failure
+//  If pii_hash does not match v4_unique_id, return failure
+//  Otherwise creates new identity and attaches CDD claim
+//
+// Assumptions:
+//  CDD provider has verified user matches pii_hash data (via documentation checks)
+//  CDD provider has verified that user owns primary_key (via random signing challenge)
+//
+// Returns:
+//  [true, did] if operation is successful, otherwise throws error
+async function existing_user_with_new_identity(primary_key, pii_hash, v4_unique_id) {
+
+    // console.log("****Input Data****");
+    // console.log(JSON.stringify({primary_key: primary_key, pii_hash: pii_hash, unique_id: v4_unique_id}, null, 2));
+
+    console.log('****Polymesh***** Getting SDK api')
+    const polymesh = await getPolymesh();    
+
+    console.log('****Polymesh***** Checking Primary Key is unused')
+    let existing_identity = await getIdentityFromAccount(polymesh, primary_key);
+    if (existing_identity != undefined) {
+        throw new Error("PrimaryKeyInUse");
+    }
+
+    console.log('****PUIS***** Getting Swagger Client');
+    let puis = await getPUIS();
+
+    // Check that verified pii data matches user provided unique id
+    console.log('****PUIS***** Checking if PII hash matches unique id');
+    let [piiMatch, provider_name, provider_url] = await piiHashMatchesUid(puis, pii_hash, v4_unique_id);
+    if (!piiMatch) {
+        throw new Error("PiiAndUniqueIdMismatch");
+    }
+    console.log('PII hash matches unique id');
+
+    let unique_id = "0x" + (v4_unique_id.replace(/-/g, ''));
+
+    console.log('****Polymesh***** Creating DID')
+    let identity = await createIdentity(polymesh, primary_key);
+    console.log("DID Created: ", identity.did);
+
+    console.log('****Confidential Identity Library***** Deriving CDD_ID');
+    let cdd_id = confidential_identity.process_create_cdd_id(
+        JSON.stringify({
+            investor_did: Array.from(hexToU8a(identity.did)),
+            investor_unique_id: Array.from(hexToU8a(unique_id))
+        })
+    );
+    cdd_id = "0x" + createHexString(JSON.parse(cdd_id).cdd_id);
+    console.log("CDD_ID Derived: ", cdd_id)
+
+    console.log('****Polymesh***** Writing CDD Attestation')
+    await writeCddClaim(polymesh, identity.did, cdd_id)
+    console.log('CDD Attestation Written');
+
+    return [true, identity.did]
+
+}
+
+async function getPolymesh() {
+    const api = await Polymesh.connect({
+        nodeUrl: 'ws://127.0.0.1:9944',
+        accountUri: '//Alice',
+    });
+    return api;
+}
+
+async function getPUIS() {
+    let swagger_file = "./swagger.json";
+    var url = path.join(__dirname, swagger_file);
+    let swagger_spec = JSON.parse(fs.readFileSync(url));
+
+    return new SwaggerClient({
+        spec: swagger_spec,
+        authorizations: { basicAuth: { username: '513dec7807e371e6aba311583521384ec9fe7e137cf6be1e01e6ca542746aac6', password: 'anypass' } }
+    });
+}
+
+async function piiHashMatchesUid(puis, pii_hash, uId) {
+    let puis_response = await puis.apis.identity.checkuID({checks: {checks: pii_hash, uID: uId}});
+    let match = puis_response.body[0];
+    if ("check_name" in match) {
+        return [true, match.CheckProvider.provider_name, match.CheckProvider.provider_url];
+    } else {
+        return [false, "", ""];
+    }
+}
+
+async function piiExists(puis, pii_hash) {
+    let puis_response = await puis.apis.identity.searchuID({body: pii_hash});
+    let match = puis_response.body[0];
+    if ("check_name" in match) {
+        return [true, match.CheckProvider.provider_name, match.CheckProvider.provider_url];
+    } else {
+        return [false, "", ""];
+    }
+}
+
+async function getUid(puis, payload) {
+    let puis_response = await puis.apis.identity.createuID({IdentityChecks: payload});
+    let uId = puis_response.body.uID;        
+    return uId;
+}
+
+async function createIdentity(polymesh, target_account) {
+    let transactionQ = await polymesh.registerIdentity({
+        targetAccount: target_account
+    });
+    const identity = await transactionQ.run();
+    return identity;
+}
+
+async function getIdentityFromAccount(polymesh, target_account) {
+    let account = await polymesh.getAccount({
+        address: target_account
+    });
+    let identity = await account.getIdentity();
+    return identity;
+}
+
+async function writeCddClaim(polymesh, did, cdd_id) {
+    let claims = polymesh.claims;
+    let transactionQ = await claims.addClaims(
+        {claims: [
+            {
+                target: did,
+                claim: {
+                    type: ClaimType.CustomerDueDiligence,
+                    id: cdd_id,
+                },
+            },
+        ]}
+    )
+    await transactionQ.run();
+}
+
+// Create dummy inputs as needed
+
+function createDummyPiiHash() {
+    return [
+        {
+          "check_name": "BaseIndividual",
+          "pii_payloads": [
+            {
+                "payload_name": "FirstName",
+                "payload_value": sha512(Math.random().toString(36).substring(7))
+            },
+            {
+                "payload_name": "LastName",
+                "payload_value": sha512(Math.random().toString(36).substring(7))
+            },
+            {
+                "payload_name": "BirthDate",
+                "payload_value": sha512(Math.random().toString(36).substring(7))
+            },
+            {
+                "payload_name": "CountryAlpha2",
+                "payload_value": "CA"
+            }
+          ]
+        }
+    ];
+}
+
+function createFixedPiiHash() {
+    return [
+        {
+          "check_name": "BaseIndividual",
+          "pii_payloads": [
+            {
+                "payload_name": "FirstName",
+                "payload_value": sha512("Adam1")
+            },
+            {
+                "payload_name": "LastName",
+                "payload_value": sha512("Dossa")
+            },
+            {
+                "payload_name": "BirthDate",
+                "payload_value": sha512("19/11/1979")
+            },
+            {
+                "payload_name": "CountryAlpha2",
+                "payload_value": "UK"
+            }
+          ]
+        }
+    ];
+}
+
+function createDummyAccount() {
+    const random_address = '0x' + crypto.randomBytes(32).toString('hex');
+    return encodeAddress(random_address, 42);
+}
+
+// Utility functions
+
+function parseHexString(str) { 
+    var result = [];
+    while (str.length >= 2) { 
+        result.push(parseInt(str.substring(0, 2), 16));
+        str = str.substring(2, str.length);
+    }
+
+    return result;
+}
+
+function createHexString(arr) {
+    var result = "";
+    for (i in arr) {
+        var str = arr[i].toString(16);
+        str = str.length == 0 ? "00" :
+              str.length == 1 ? "0" + str : 
+              str.length == 2 ? str :
+              str.substring(str.length-2, str.length);
+        result += str;
+    }
+    return result;
+}
+
+async function main() {
+    let primary_key = createDummyAccount();
+    let pii_hash = createDummyPiiHash();
+    let uuid;
+    let did;
+    let result;
+
+    // Onboard a new user
+    console.log("Creating New User: \n", JSON.stringify({primary_key: primary_key, pii_hash: pii_hash}, null, 2));
+    try {
+        [result, did, uuid] = await new_user(primary_key, pii_hash);
+        if (!result) {
+            throw new Error("NewUserUnexpectedError");
+        }
+        console.log("New User: ", did, uuid);
+    } catch (err) {
+        console.log("Unexpected Error: ", err);
+        return;
+    }
+
+    let new_primary_key = createDummyAccount();
+    console.log("Redirecting New User: \n", JSON.stringify({primary_key: new_primary_key, pii_hash: pii_hash}, null, 2));
+    try {
+        let provider_name;
+        let provider_url;
+        [result, provider_name, provider_url] = await new_user(new_primary_key, pii_hash);
+        if (result) {
+            throw new Error("RedirectedUserUnexpectedError");
+        }
+        console.log("Redirected User: ", provider_name, provider_url);
+    } catch (err) {
+        console.log("Unexpected Error: ", err);
+        return;
+    }
+
+    // Create second CDD claim on existing identity
+    console.log("Creating Second Claim On Existing DID: \n", JSON.stringify({primary_key: primary_key, pii_hash: pii_hash, uuid: uuid, did: did}, null, 2));
+    try {
+        result = await existing_user_with_existing_identity(primary_key, pii_hash, uuid, did);
+        if (!result) {
+            throw new Error("ExistingUserExistingIdentityUnexpectedError");
+        }
+        console.log("Existing User, Existing Identity: ", did, uuid)
+    } catch (err) {
+        console.log("Unexpected Error: ", err);
+        return;
+    }
+
+    // Create second CDD claim on new identity
+    let new_did;
+    console.log("Creating Second Claim On New DID: \n", JSON.stringify({primary_key: new_primary_key, pii_hash: pii_hash, uuid: uuid}, null, 2));
+    try {
+        [result, new_did] = await existing_user_with_new_identity(new_primary_key, pii_hash, uuid);
+        if (!result) {
+            throw new Error("ExistingUserNewIdentityUnexpectedError");
+        }
+        console.log("Existing User, New Identity: ", did, uuid)
+    } catch (err) {
+        console.log("Unexpected Error: ", err);
+        return;
+    }
+}
+
+main();
